058a596583ff8dcc385832dc003bea35c8b60ff9
FedericoManzano <federicomanzano111@gmail.com>
2017-11-24 12:41:22 -0300

    Mejora del diseño pasaje de metodos a la clase ModoJuego y javadoc

--- src/main/java/edu/unlam/wome/modos/ModoDios.java
+++ src/main/java/edu/unlam/wome/modos/ModoDios.java
@@ -1,108 +1,33 @@
 package edu.unlam.wome.modos;
-
-import java.io.IOException;
-import java.util.Map;
-
-import com.google.gson.Gson;
-
-import edu.unlam.wome.mensajeria.Comando;
 import edu.unlam.wome.mensajeria.PaqueteMensaje;
 import edu.unlam.wome.mensajeria.PaqueteModoJuego;
-import edu.unlam.wome.mensajeria.PaquetePersonaje;
 import edu.unlam.wome.potenciados.PersonajesPotenciados;
-import edu.unlam.wome.servidor.EscuchaCliente;
 import edu.unlam.wome.servidor.Servidor;
 
-
+/**
+ * Clase que extiende de ModoJuego que permite cargar la funcionalidad
+ * modo dios a los personajes
+ * @see edu.unlam.wome.modos.ModoJuego
+ *
+ */
 public class ModoDios extends ModoJuego{
 	private PaqueteMensaje paqueteMensaje;
-	private Gson gson = new Gson();
-	
 	
 	public ModoDios(PaqueteMensaje paqueteMensaje) {
 		this.paqueteMensaje = paqueteMensaje;
 	}
 	
-	
+	/**
+	 * Metodo sobrescrito que possen todas las clases derivadas de ModoJuego
+	 */
 	@Override
 	public boolean actualizar() {
 		int idPersonaje = buscarIdPersobaje(paqueteMensaje, paqueteMensaje.getUserEmisor());
-		PaqueteModoJuego paqueteModoJuego = configurarPaquete(idPersonaje);
+		PaqueteModoJuego paqueteModoJuego = configurarPaquete(idPersonaje, PaqueteModoJuego.MODO_DIOS);
 		actualizarModoJuegoAlJugador(idPersonaje, paqueteModoJuego);
 		Servidor.potenciados.add(new PersonajesPotenciados(idPersonaje, PaqueteModoJuego.MODO_DIOS));
 		enviarMensaje(paqueteMensaje);
 		return actualizarPotenciasdosATodos(paqueteMensaje, paqueteModoJuego);	
 		
 	}
-
-	/**
-     * Devuelve el id del personaje que realizo el truco
-     * @param paqueteMensaje
-     * @param modoJuego
-     * @return idUsuario que realizo el truco
-     */
-	public int buscarIdPersobaje(PaqueteMensaje paqueteMensaje, String usuarioBuscar) {
-		int idUser = 0;
-		for (Map.Entry<Integer, PaquetePersonaje> personaje : Servidor.getPersonajesConectados().entrySet()) {
-			if (personaje.getValue().getNombre().equals(usuarioBuscar)) {
-				idUser = personaje.getValue().getId();
-				return idUser;
-			}
-		}
-		return idUser;
-	}
-	
-	 /**
-     * Envia a todos los conectados el paquete que contiene al jugador que realizo el truco
-     * @param paqueteMensaje
-     * @param paqueteModoJuego
-     * @return true /  false
-     */
-	public boolean actualizarPotenciasdosATodos(PaqueteMensaje paqueteMensaje, PaqueteModoJuego paqueteModoJuego) {
-		paqueteModoJuego.setComando(Comando.ACTUALIZAR_MODO_JUEGO);
-		for (EscuchaCliente conectado : Servidor.getClientesConectados()) {
-              try {
-                   conectado.getSalida().writeObject(gson.toJson(paqueteModoJuego));
-               } catch (IOException e) {
-                    Servidor.getLog().append(
-                            "Falló al intentar enviar mensaje a:" + conectado.getPaquetePersonaje().getId() + "\n");
-                    return false;
-               }
-        }
-        return true;
-    }
-	
-	
-	/**
-     * Envia el mensaje al 
-     * @param paqueteMensaje
-     */
-	public void enviarMensaje(PaqueteMensaje paqueteMensaje) {
-    	int idUser= 0;
-    	paqueteMensaje.setComando(Comando.TALK);
-    	paqueteMensaje.setMensaje("El Truco fue aprobado");
-        idUser = buscarIdPersobaje(paqueteMensaje, paqueteMensaje.getUserEmisor());
-        for (EscuchaCliente conectado : Servidor.getClientesConectados()) {
-            if (conectado.getIdPersonaje() == idUser) {
-                try {
-                    conectado.getSalida().writeObject(gson.toJson(paqueteMensaje));
-                } catch (IOException e) {
-                    Servidor.getLog().append("Falló al intentar enviar mensaje a:"
-                            + conectado.getPaquetePersonaje().getId() + "\n");
-                }
-            }
-        }
-    }
-	
-	private PaqueteModoJuego configurarPaquete(int idPersonaje) {
-		PaqueteModoJuego paqueteModoJuego = new PaqueteModoJuego();
-		paqueteModoJuego.setIdPersonaje(idPersonaje);
-		paqueteModoJuego.setModo(PaqueteModoJuego.MODO_DIOS);
-		return paqueteModoJuego;
-	}
-	
-	private void actualizarModoJuegoAlJugador(int idPersonaje, PaqueteModoJuego paqueteModoJuego) {
-		Servidor.getPersonajesConectados().get(idPersonaje).setModoJuego(paqueteModoJuego.getModo());
-	}
-
 }
--- src/main/java/edu/unlam/wome/modos/ModoJuego.java
+++ src/main/java/edu/unlam/wome/modos/ModoJuego.java
@@ -1,8 +1,26 @@
 package edu.unlam.wome.modos;
 
-import edu.unlam.wome.mensajeria.PaqueteMensaje;
+import java.io.IOException;
+import java.util.Map;
 
+import com.google.gson.Gson;
+
+import edu.unlam.wome.mensajeria.Comando;
+import edu.unlam.wome.mensajeria.PaqueteMensaje;
+import edu.unlam.wome.mensajeria.PaqueteModoJuego;
+import edu.unlam.wome.mensajeria.PaquetePersonaje;
+import edu.unlam.wome.servidor.EscuchaCliente;
+import edu.unlam.wome.servidor.Servidor;
+
+
+/**
+ * Clase abstrata que provee las funcionalidades necesarias para cargar trucos
+ * @see edu.unlam.wome.modos.ModoDios
+ *
+ */
 public abstract class ModoJuego {
+	private Gson gson = new Gson();
+	
 	
 	public ModoJuego seleccion(PaqueteMensaje paqueteMensaje) {
 		switch(paqueteMensaje.getMensaje()) {
@@ -13,4 +31,80 @@ public abstract class ModoJuego {
 	
 	public abstract boolean actualizar();
 	
+	
+	/**
+     * Devuelve el id del personaje que realizo el truco
+     * @param paqueteMensaje
+     * @param modoJuego
+     * @return idUsuario que realizo el truco
+     */
+	public int buscarIdPersobaje(PaqueteMensaje paqueteMensaje, String usuarioBuscar) {
+		int idUser = 0;
+		for (Map.Entry<Integer, PaquetePersonaje> personaje : Servidor.getPersonajesConectados().entrySet()) {
+			if (personaje.getValue().getNombre().equals(usuarioBuscar)) {
+				idUser = personaje.getValue().getId();
+				return idUser;
+			}
+		}
+		return idUser;
+	}
+	
+	 /**
+     * Envia a todos los conectados el paquete que contiene al jugador que realizo el truco
+     * @param paqueteMensaje
+     * @param paqueteModoJuego
+     * @return true /  false
+     */
+	public boolean actualizarPotenciasdosATodos(PaqueteMensaje paqueteMensaje, PaqueteModoJuego paqueteModoJuego) {
+		paqueteModoJuego.setComando(Comando.ACTUALIZAR_MODO_JUEGO);
+		for (EscuchaCliente conectado : Servidor.getClientesConectados()) {
+              try {
+                   conectado.getSalida().writeObject(gson.toJson(paqueteModoJuego));
+               } catch (IOException e) {
+                    Servidor.getLog().append(
+                            "Falló al intentar enviar mensaje a:" + conectado.getPaquetePersonaje().getId() + "\n");
+                    return false;
+               }
+        }
+        return true;
+    }
+	
+	
+	/**
+     * Envia el mensaje al 
+     * @param paqueteMensaje
+     */
+	public void enviarMensaje(PaqueteMensaje paqueteMensaje) {
+    	int idUser= 0;
+    	paqueteMensaje.setComando(Comando.TALK);
+    	paqueteMensaje.setMensaje("El Truco fue aprobado");
+        idUser = buscarIdPersobaje(paqueteMensaje, paqueteMensaje.getUserEmisor());
+        for (EscuchaCliente conectado : Servidor.getClientesConectados()) {
+            if (conectado.getIdPersonaje() == idUser) {
+                try {
+                    conectado.getSalida().writeObject(gson.toJson(paqueteMensaje));
+                } catch (IOException e) {
+                    Servidor.getLog().append("Falló al intentar enviar mensaje a:"
+                            + conectado.getPaquetePersonaje().getId() + "\n");
+                }
+            }
+        }
+    }
+	
+	/**
+	 * Carga el paquete con los datos solicitados por el personaje
+	 * @param idPersonaje
+	 * @param modo
+	 * @return PaqueteModoJuego
+	 */
+	public PaqueteModoJuego configurarPaquete(int idPersonaje, int modo) {
+		PaqueteModoJuego paqueteModoJuego = new PaqueteModoJuego();
+		paqueteModoJuego.setIdPersonaje(idPersonaje);
+		paqueteModoJuego.setModo(PaqueteModoJuego.MODO_DIOS);
+		return paqueteModoJuego;
+	}
+	
+	public void actualizarModoJuegoAlJugador(int idPersonaje, PaqueteModoJuego paqueteModoJuego) {
+		Servidor.getPersonajesConectados().get(idPersonaje).setModoJuego(paqueteModoJuego.getModo());
+	}
 }
